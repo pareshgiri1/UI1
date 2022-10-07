07e51634d055bd01b6f3b975f0e8024ce6e65571
pareshgiri1 <pareshgiri.aparanathi@yudiz.com>
2022-10-07 10:17:54 +0530

    commit

--- Assets/_Scripts/UiAnimatePosition.cs
+++ Assets/_Scripts/UiAnimatePosition.cs
@@ -11,36 +11,15 @@ public class UiAnimatePosition : MonoBehaviour
     public RectTransform rect;
     Vector3 startPosition;
     //public Vector2 positions;
+    private void Start()
+    {
+        StartCoroutine(Animation());
+    }
     public Vector3 targetPosition;
     IEnumerator Animation()
     {
-        #region if
-        //if (y > 0)
-        //{
-        //    while (y > 0)
-        //    {
-        //        y -= change * 2;
-        //        shopRect.anchoredPosition3D = new Vector3(shopRect.anchoredPosition3D.x, y, shopRect.anchoredPosition3D.z);
-        //        Debug.Log(y);
-        //        yield return new WaitForSeconds(0.1f);
-        //    }
-        //}
-        //else if (y <= 0)
-        //{
-        //    while (y < Screen.height)
-        //    {
-        //        y += change * 2;
-        //        shopRect.anchoredPosition3D = new Vector3(shopRect.anchoredPosition3D.x, y, shopRect.anchoredPosition3D.z);
-        //        Debug.Log(y);
-        //        yield return new WaitForSeconds(0.1f);
-        //    }
-
-        //}
-        #endregion
         timer = 0;
         startPosition = rect.anchoredPosition3D;
-        
-            //targetPosition = new Vector3(targetPositionX, rect.anchoredPosition3D.y, rect.anchoredPosition3D.z);
         
         while (timer < estimate)
         {
