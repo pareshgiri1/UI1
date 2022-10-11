76836a08d354c774bbb5135587a001409a829d5b
pareshgiri1 <pareshgiri.aparanathi@yudiz.com>
2022-10-11 17:26:56 +0530

    commit

--- Assets/UI_Scripts/UI_Animate/AnimationBase.cs
+++ Assets/UI_Scripts/UI_Animate/AnimationBase.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public abstract class AnimationBase : MonoBehaviour
 {
     public float showTime = 0.5f;
+    [HideInInspector]
     public float hideTime = 0.5f;
+    [HideInInspector]
     public float timer = 0f;
     public AnimationCurve animationCurve;
     public abstract void ShowAnimation();
